14fcfd3c6de980220c181ef753aa97421757d0a4
S7245 <samlau0401@gmail.com>
2023-11-06 16:42:52 +0800

    更新

---
+++ src/components/CoolImage/index.js
@@ -0,0 +1,10 @@
+import styles from './index.module.css';
+
+export default function CoolImage() {
+  return (
+    <img
+        src="https://blogs7245-1256587996.cos.ap-guangzhou.myqcloud.com/img/EUse-pic-0000110.png"
+        className={styles.features}
+      />
+  );
+}
